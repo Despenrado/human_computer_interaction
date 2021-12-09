00f487827b03a3116819c25d0376511c8a053a29
Nikita Stepanenko <StepNik007@gmail.com>
2021-12-09 12:26:05 +0100

    spechRec

---
+++ SpeechRec/recognizer.py
@@ -0,0 +1,209 @@
+import os
+import pathlib
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import tensorflow as tf
+import torch
+
+from tensorflow.keras import layers
+from tensorflow.keras import models
+from IPython import display
+
+# Set the seed value for experiment reproducibility.
+seed = 42
+tf.random.set_seed(seed)
+np.random.seed(seed)
+
+DATASET_PATH = 'data/mini_speech_commands'
+
+data_dir = pathlib.Path(DATASET_PATH)
+
+with open('commands.json') as json_file:
+    commands = json.load(json_file)
+commands = np.array(commands)
+print('Commands:', commands)
+
+filenames = tf.io.gfile.glob(str(data_dir) + '/*/*')
+filenames = tf.random.shuffle(filenames)
+num_samples = len(filenames)
+print('Number of total examples:', num_samples)
+print('Number of examples per label:',
+      len(tf.io.gfile.listdir(str(data_dir / commands[0]))))
+print('Example file tensor:', filenames[0])
+
+train_files = filenames[:6400]
+val_files = filenames[6400: 6400 + 800]
+test_files = filenames[-800:]
+
+print('Training set size', len(train_files))
+print('Validation set size', len(val_files))
+print('Test set size', len(test_files))
+
+test_file = tf.io.read_file(DATASET_PATH + '/czas/Record (online-voice-recorder.com) (86).wav')
+test_audio, _ = tf.audio.decode_wav(contents=test_file)
+test_audio.shape
+
+
+def decode_audio(audio_binary):
+    # Decode WAV-encoded audio files to `float32` tensors, normalized
+    # to the [-1.0, 1.0] range. Return `float32` audio and a sample rate.
+    audio, _ = tf.audio.decode_wav(contents=audio_binary)
+    # Since all the data is single channel (mono), drop the `channels`
+    # axis from the array.
+    return tf.squeeze(audio, axis=-1)
+
+
+def get_label(file_path):
+    parts = tf.strings.split(
+        input=file_path,
+        sep=os.path.sep)
+    # Note: You'll use indexing here instead of tuple unpacking to enable this
+    # to work in a TensorFlow graph.
+    return parts[-2]
+
+
+def get_waveform_and_label(file_path):
+    label = get_label(file_path)
+    audio_binary = tf.io.read_file(file_path)
+    waveform = decode_audio(audio_binary)
+    return waveform, label
+
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+files_ds = tf.data.Dataset.from_tensor_slices(train_files)
+
+waveform_ds = files_ds.map(
+    map_func=get_waveform_and_label,
+    num_parallel_calls=AUTOTUNE)
+
+
+def get_spectrogram(waveform):
+    # Zero-padding for an audio waveform with less than 16,000 samples.
+    input_len = 16000
+    waveform = waveform[:input_len]
+    zero_padding = tf.zeros(
+        [16000] - tf.shape(waveform),
+        dtype=tf.float32)
+    # Cast the waveform tensors' dtype to float32.
+    waveform = tf.cast(waveform, dtype=tf.float32)
+    # Concatenate the waveform with `zero_padding`, which ensures all audio
+    # clips are of the same length.
+    equal_length = tf.concat([waveform, zero_padding], 0)
+    # Convert the waveform to a spectrogram via a STFT.
+    spectrogram = tf.signal.stft(
+        equal_length, frame_length=255, frame_step=128)
+    # Obtain the magnitude of the STFT.
+    spectrogram = tf.abs(spectrogram)
+    # Add a `channels` dimension, so that the spectrogram can be used
+    # as image-like input data with convolution layers (which expect
+    # shape (`batch_size`, `height`, `width`, `channels`).
+    spectrogram = spectrogram[..., tf.newaxis]
+    return spectrogram
+
+
+for waveform, label in waveform_ds.take(1):
+    label = label.numpy().decode('utf-8')
+    spectrogram = get_spectrogram(waveform)
+
+print('Label:', label)
+print('Waveform shape:', waveform.shape)
+print('Spectrogram shape:', spectrogram.shape)
+print('Audio playback')
+display.display(display.Audio(waveform, rate=16000))
+
+
+def get_spectrogram_and_label_id(audio, label):
+    spectrogram = get_spectrogram(audio)
+    label_id = tf.argmax(label == commands)
+    return spectrogram, label_id
+
+
+spectrogram_ds = waveform_ds.map(
+    map_func=get_spectrogram_and_label_id,
+    num_parallel_calls=AUTOTUNE)
+
+
+def preprocess_dataset(files):
+    files_ds = tf.data.Dataset.from_tensor_slices(files)
+    output_ds = files_ds.map(
+        map_func=get_waveform_and_label,
+        num_parallel_calls=AUTOTUNE)
+    output_ds = output_ds.map(
+        map_func=get_spectrogram_and_label_id,
+        num_parallel_calls=AUTOTUNE)
+    return output_ds
+
+
+train_ds = spectrogram_ds
+val_ds = preprocess_dataset(val_files)
+test_ds = preprocess_dataset(test_files)
+
+batch_size = 64
+train_ds = train_ds.batch(batch_size)
+val_ds = val_ds.batch(batch_size)
+
+train_ds = train_ds.cache().prefetch(AUTOTUNE)
+val_ds = val_ds.cache().prefetch(AUTOTUNE)
+
+for spectrogram, _ in spectrogram_ds.take(1):
+    input_shape = spectrogram.shape
+print('Input shape:', input_shape)
+num_labels = len(commands)
+
+# Instantiate the `tf.keras.layers.Normalization` layer.
+norm_layer = layers.Normalization()
+# Fit the state of the layer to the spectrograms
+# with `Normalization.adapt`.
+norm_layer.adapt(data=spectrogram_ds.map(map_func=lambda spec, label: spec))
+
+model = tf.keras.models.load_model('model')
+# metrics = history.history
+# plt.plot(history.epoch, metrics['loss'], metrics['val_loss'])
+# plt.legend(['loss', 'val_loss'])
+# plt.show()
+
+
+test_audio = []
+test_labels = []
+
+for audio, label in test_ds:
+    test_audio.append(audio.numpy())
+    test_labels.append(label.numpy())
+
+test_audio = np.array(test_audio)
+test_labels = np.array(test_labels)
+
+y_pred = np.argmax(model.predict(test_audio), axis=1)
+y_true = test_labels
+
+test_acc = sum(y_pred == y_true) / len(y_true)
+print(f'Test set accuracy: {test_acc:.0%}')
+
+confusion_mtx = tf.math.confusion_matrix(y_true, y_pred)
+plt.figure(figsize=(10, 8))
+sns.heatmap(confusion_mtx,
+            xticklabels=commands,
+            yticklabels=commands,
+            annot=True, fmt='g')
+plt.xlabel('Prediction')
+plt.ylabel('Label')
+plt.show()
+
+sample_file = data_dir / 'otworz/84.wav'
+
+sample_ds = preprocess_dataset([str(sample_file)])
+
+for spectrogram, label in sample_ds.batch(1):
+    prediction = model(spectrogram)
+    plt.bar(commands, tf.nn.softmax(prediction[0]))
+    plt.title(f'Predictions for "{commands[label[0]]}"')
+    plt.show()
+
+
+
+
+
---
+++ SpeechRec/recognizer2.py
@@ -0,0 +1,115 @@
+import os
+import pathlib
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import tensorflow as tf
+import torch
+
+from tensorflow.keras import layers
+from tensorflow.keras import models
+from IPython import display
+
+# Set the seed value for experiment reproducibility
+
+DATASET_PATH = 'data/mini_speech_commands'
+
+data_dir = pathlib.Path(DATASET_PATH)
+
+with open('commands.json') as json_file:
+    commands = json.load(json_file)
+commands = np.array(commands)
+print('Commands:', commands)
+
+
+def decode_audio(audio_binary):
+    # Decode WAV-encoded audio files to `float32` tensors, normalized
+    # to the [-1.0, 1.0] range. Return `float32` audio and a sample rate.
+    audio, _ = tf.audio.decode_wav(contents=audio_binary)
+    # Since all the data is single channel (mono), drop the `channels`
+    # axis from the array.
+    return tf.squeeze(audio, axis=-1)
+
+
+def get_label(file_path):
+    parts = tf.strings.split(
+        input=file_path,
+        sep=os.path.sep)
+    # Note: You'll use indexing here instead of tuple unpacking to enable this
+    # to work in a TensorFlow graph.
+    return parts[-2]
+
+
+def get_waveform_and_label(file_path):
+    label = get_label(file_path)
+    audio_binary = tf.io.read_file(file_path)
+    waveform = decode_audio(audio_binary)
+    return waveform, label
+
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+
+def get_spectrogram(waveform):
+    # Zero-padding for an audio waveform with less than 16,000 samples.
+    input_len = 16000
+    waveform = waveform[:input_len]
+    zero_padding = tf.zeros(
+        [16000] - tf.shape(waveform),
+        dtype=tf.float32)
+    # Cast the waveform tensors' dtype to float32.
+    waveform = tf.cast(waveform, dtype=tf.float32)
+    # Concatenate the waveform with `zero_padding`, which ensures all audio
+    # clips are of the same length.
+    equal_length = tf.concat([waveform, zero_padding], 0)
+    # Convert the waveform to a spectrogram via a STFT.
+    spectrogram = tf.signal.stft(
+        equal_length, frame_length=255, frame_step=128)
+    # Obtain the magnitude of the STFT.
+    spectrogram = tf.abs(spectrogram)
+    # Add a `channels` dimension, so that the spectrogram can be used
+    # as image-like input data with convolution layers (which expect
+    # shape (`batch_size`, `height`, `width`, `channels`).
+    spectrogram = spectrogram[..., tf.newaxis]
+    return spectrogram
+
+
+def get_spectrogram_and_label_id(audio, label):
+    spectrogram = get_spectrogram(audio)
+    label_id = tf.argmax(label == commands)
+    return spectrogram, label_id
+
+
+
+def preprocess_dataset(files):
+    files_ds = tf.data.Dataset.from_tensor_slices(files)
+    output_ds = files_ds.map(
+        map_func=get_waveform_and_label,
+        num_parallel_calls=AUTOTUNE)
+    output_ds = output_ds.map(
+        map_func=get_spectrogram_and_label_id,
+        num_parallel_calls=AUTOTUNE)
+    return output_ds
+
+model = tf.keras.models.load_model('model')
+# metrics = history.history
+# plt.plot(history.epoch, metrics['loss'], metrics['val_loss'])
+# plt.legend(['loss', 'val_loss'])
+# plt.show()
+
+sample_file = data_dir / 'otworz/11.wav'
+
+sample_ds = preprocess_dataset([str(sample_file)])
+
+for spectrogram, label in sample_ds.batch(1):
+    prediction = model(spectrogram)
+    plt.bar(commands, tf.nn.softmax(prediction[0]))
+    plt.title(f'Predictions for "{commands[label[0]]}"')
+    plt.show()
+
+
+
+
+
